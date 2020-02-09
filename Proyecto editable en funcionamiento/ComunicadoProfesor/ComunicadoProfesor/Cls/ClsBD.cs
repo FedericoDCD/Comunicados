5e32f2d6790e17d4007f7c237de0766c7241a81f
FedericoDCDC <magos.aq@gmail.com>
2020-02-08 22:24:26 -0300

    Proyecto con ejecutables online y aviso de errores de BD

--- Proyecto editable en funcionamiento/ComunicadoProfesor/ComunicadoProfesor/Cls/ClsBD.cs
+++ Proyecto editable en funcionamiento/ComunicadoProfesor/ComunicadoProfesor/Cls/ClsBD.cs
@@ -11,7 +11,8 @@ namespace ComunicadoProfesor.Cls
     class ClsBD
     {
         //Guardo la stringconnection y realizo las conexiones necesarias y métodos para mejorar su uso desde otras clases
-        string StringConnection = "Data Source=(local);Initial Catalog = BDComunicacion; Integrated Security = True";
+        //string StringConnection = "Data Source=(local);Initial Catalog = BDComunicacion; Integrated Security = True";
+        string StringConnection = "Data Source = SQL5045.site4now.net; Initial Catalog = DB_A550BE_BDComunicados; User Id = DB_A550BE_BDComunicados_admin; Password=2Comunidad;";
         public SqlConnection Conn = new SqlConnection();
         public ClsBD()
         {
